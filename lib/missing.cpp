e71c5fabc4d81ebffe63d3cb9b68a4c4cda5d80c
Matthias Kramm <kramm@quiss.org>
2010-12-27 14:54:09 -0800

    made local copy of opencv sources

---
+++ lib/missing.cpp
@@ -0,0 +1,17 @@
+#include "precomp_core.hpp"
+
+namespace cv
+{
+
+    bool eigen( const Mat& src, Mat& evals, int lowindex, int highindex )
+    {
+        fprintf(stderr, "Error: cv::eigen() not implemented");
+        return false;
+    }
+
+    bool eigen( const Mat& src, Mat& evals, Mat& evects, int lowindex, int highindex )
+    {
+        fprintf(stderr, "Error: cv::eigen() not implemented");
+        return false;
+    }
+};
