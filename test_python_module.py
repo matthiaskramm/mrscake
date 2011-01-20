e05dbdd278731ab5d4c7796c2d03a4da8e6c3d3a
Matthias Kramm <kramm@quiss.org>
2011-01-19 18:14:43 -0800

    fixed python test code

--- test_python_module.py
+++ test_python_module.py
@@ -1,19 +1,12 @@
 import predict
 
 data = predict.DataSet()
-#data.train([2,1.0,5,1.0,"A",7], output="yes")
-#data.train([1,1.3,3,1.0,"B",7], output="yes")
-#data.train([2,0.3,3,0.0,"A",7], output="no")
-#data.train([1,1.1,3,0.5,"C",7], output="no")
-#data.train([2,0.7,3,1.0,"A",7], output="yes")
-#data.train([1,1.1,5,0.0,"B",7], output="no")
-
-data.train([1.0], output="yes")
-data.train([1.0], output="yes")
-data.train([0.0], output="no")
-data.train([0.5], output="no")
-data.train([1.0], output="yes")
-data.train([0.0], output="no")
+data.train([2,1.0,5,1.0,"A",7], output="yes")
+data.train([1,1.3,3,1.0,"B",7], output="yes")
+data.train([2,0.3,3,0.0,"A",7], output="no")
+data.train([1,1.1,3,0.5,"C",7], output="no")
+data.train([2,0.7,3,1.0,"A",7], output="yes")
+data.train([1,1.1,5,0.0,"B",7], output="no")
 print data
 model = data.get_model()
 print model
