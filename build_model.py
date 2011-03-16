76948d65e2758fcce5ced97f0565f6111faf9498
Matthias Kramm <kramm@quiss.org>
2011-03-16 09:06:07 -0700

    added header detection to dataset parsing

--- build_model.py
+++ build_model.py
@@ -73,6 +73,32 @@ with open(filename, "rb") as fi:
                 items[-1] += seperator_char + item
         data.append(items)
 
+# header detection
+
+headers = None
+NUMBER = re.compile("([0-9]+|[0-9]*[.][0-9]*(e[0-9]+)?)")
+IDENTIFIER = re.compile("([a-zA-Z_][a-zA-Z0-9_]*)")
+numbers_on_first_line = 0
+numbers_on_second_line = 0
+identifiers_on_first_line = 0
+identifiers_on_second_line = 0
+for item1,item2 in zip(data[0],data[1]):
+    if NUMBER.match(item1):
+        numbers_on_first_line += 1
+    if IDENTIFIER.match(item1):
+        identifiers_on_first_line += 1
+    if NUMBER.match(item2):
+        numbers_on_second_line += 1
+    if IDENTIFIER.match(item2):
+        identifiers_on_second_line += 1
+if numbers_on_first_line < numbers_on_second_line and \
+   identifiers_on_first_line > identifiers_on_second_line:
+       print "has column headers"
+       headers = data[0]
+       data = data[1:]
+
+# target column detection
+
 first_column_values = {}
 last_column_values = {}
 first_column_floats = 0
@@ -116,10 +142,20 @@ for i in range(len(data)):
     output = convert_value(output)
     examples += [[inputs,output]]
 
-import predict
-dataset = predict.DataSet()
-for inputs,output in examples:
-    dataset.train(inputs, output=output)
+if headers:
+    if target_variable_is_last_column:
+        headers = headers[0:-1]
+    else:
+        headers = headers[1:]
+
+import mrscake
+dataset = mrscake.DataSet()
+if headers:
+    for inputs,output in examples:
+        dataset.add(dict(zip(headers,inputs)), output=output)
+else:
+    for inputs,output in examples:
+        dataset.add(inputs, output=output)
 
 output_filename = "test.model"
 model = dataset.get_model()
